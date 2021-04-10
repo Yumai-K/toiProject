49f34df141f37533837a2a31224576142bcfea8d
DESKTOP-JUCAMKJ\song93 <song931229@naver.com>
2021-04-10 16:30:44 +0900

    SQL로그

--- src/main/java/com/toi/proto/controller/BcUsrController.java
+++ src/main/java/com/toi/proto/controller/BcUsrController.java
@@ -2,6 +2,8 @@ package com.toi.proto.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.toi.proto.dvo.BcUsrDVO;
 import com.toi.proto.service.BcUsrService;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
+@Slf4j
 public class BcUsrController {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	private BcUsrService bcUsrService;
 	
@@ -28,13 +36,14 @@ public class BcUsrController {
     
 	@GetMapping("")
     public List<BcUsrDVO> getAllUsers() {
+		logger.info("getAllUsers");
     	return bcUsrService.selectListBcUsr();
     }
 	
     
     @GetMapping("/{usrId}")
     public BcUsrDVO getUserByUsrId(String usrId) {
-    	
+    	logger.info("getUserByUsrId");
     	BcUsrDVO bcUsrDVO = new BcUsrDVO();
     	bcUsrDVO.setUsrId(usrId);
         return bcUsrService.selectOneBcUsr(bcUsrDVO);
@@ -42,17 +51,19 @@ public class BcUsrController {
  
     @PostMapping("")
     public void addUser(@RequestBody BcUsrDVO bcUsrDVO) {
-    	
+    	logger.info("addUser");
     	bcUsrService.insertBcUsr(bcUsrDVO);
     }
  
     @PutMapping("/{usrId}")
     public int setUser(@PathVariable  String usrid, @RequestBody BcUsrDVO bcUsrDVO) {
+      	logger.info("setUser");
     	return bcUsrService.updateBcUsr(bcUsrDVO);
     }
  
     @DeleteMapping("/usrid")
     public int popUser(@PathVariable  String usrId) {
+    	logger.info("popUser");
     	BcUsrDVO bcUsrDVO = new BcUsrDVO();
     	bcUsrDVO.setUsrId(usrId);
     	return bcUsrService.deleteBcUsr(bcUsrDVO);
@@ -60,6 +71,7 @@ public class BcUsrController {
     
     @GetMapping("/login")
     public String tryLogin(@RequestBody BcUsrDVO bcUsrDVO) {
+    	logger.info("login");
     	return bcUsrService.tryLogin(bcUsrDVO);
     }
 }
